a5c3cb3b6d3332d68f7098162ee0a5fec511e72d
PomHaffs <tomashaffenden@icloud.com>
2016-08-11 11:04:30 +1000

    Game nearly complete

--- Tomas_Haffendon/week_03/day_01/main.js
+++ Tomas_Haffendon/week_03/day_01/main.js
@@ -1,5 +1,62 @@
 console.log("Tic tat noooooooo!");
 
+//Audio background
+var backgroundAudio = new Audio("audio/forest.mp3");
+backgroundAudio.play();
+backgroundAudio.loop = true;
+backgroundAudio.volume = 0.4;
+
+var clapAudio = new Audio("audio/clap.mp3");
+clapAudio.volume = 0.3;
+
+var boo = new Audio("audio/boo.mp3");
+
+window.setTimeout(function() {
+final.play();
+} , 500);
+
+//random no for audio selection
+var randomAudio = function() {
+	var randomNumber = Math.floor(Math.random()*(audioFiles.length));
+	audioFiles[randomNumer].play();
+	audioFiles.pop(audioFiles[randomNumber]);
+	// Then remove the element associated with that index
+	// Then play the track that was found
+};
+
+var final = new Audio("audio/Final.m4a");
+var badgerIntro = new Audio("audio/Badgerintro.m4a");
+var wolfIntro = new Audio("audio/WolfIntro.m4a");
+var bWin = new Audio("audio/BWin.m4a");
+var wWin = new Audio("audio/WWin.m4a");
+var panda = new Audio("audio/PANDA.mp4");
+var winner = new Audio("audio/WINNER.m4a");
+
+var audioFiles = [
+new Audio("audio/BOLD.m4a"),
+new Audio("audio/CAR.m4a"),
+new Audio("audio/DANCE.m4a"),
+new Audio("audio/DOG.m4a"),
+new Audio("audio/HUMAN.m4a"),
+new Audio("audio/ITS.m4a"),
+new Audio("audio/LEGS.m4a"),
+new Audio("audio/LOVELY.m4a"),
+new Audio("audio/PAY.m4a"),
+new Audio("audio/SENTANCE.m4a"),
+new Audio("audio/SHAVE.m4a"),
+new Audio("audio/WHERE.m4a"),
+new Audio("audio/BITE.m4a"),
+new Audio("audio/FUCK.m4a"),
+new Audio("audio/HAIRY.m4a"),
+new Audio("audio/LAD.m4a"),
+new Audio("audio/LADDER.m4a"),
+new Audio("audio/NIPPLES.m4a"),
+new Audio("audio/OXY.m4a"),
+new Audio("audio/HOME.m4a"),
+new Audio("audio/GOOD.m4a")
+];
+
+
 $("document").ready(function(){
 //self populating arrays for players
 var playerOneMoves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
@@ -8,20 +65,6 @@ var playerTwoMoves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 var winningCombo = [[0, 1, 2], [0, 4, 8], [0, 3, 6],
 [1, 4, 7], [2, 5, 8], [2, 4, 6],
 [3, 4, 5], [6, 7, 8]];
-//Bonus stuff for later
-// var playerFacts = {
-// 	playerOne: {
-// 		name: "",
-// 		wins: 0,
-// 		avatar: ""
-// 	},
-// 	playerTwo: {
-// 		name: "",
-// 		wins: 0,
-// 		avatar: ""
-// 	}
-// };
-
 
 var player ="";
 var noOfMoves = 1;
@@ -50,21 +93,20 @@ $(".cell").on("click", function(event) {
 		var positionInArray = $(this).data("position");
 		var value = $(this).data("value");
 		player[positionInArray] = value;
-		//am i duplicating here?
-		console.log("THIS JUST RAN");
-		if (noOfMoves % 2 === 0) {
-			$(this).addClass('badger animated zoomIn');
-		}
-		else if (noOfMoves % 2 !== 0) {
-		 	$(this).addClass('wolf animated zoomIn');
-	 	}
-
-		if ( checkWinner() ) {
-			return false;
+		// var randomAudio = randomAudio();
+		// var audio = audioFiles[randomAudio];
+		// audio.play();
+			if (noOfMoves % 2 === 0) {
+				$(this).addClass('badger animated zoomIn');
+			}
+			else if (noOfMoves % 2 !== 0) {
+		 		$(this).addClass('wolf animated zoomIn');
+	 		}
+			if ( checkWinner() ) {
+				return false;
 		}
 	}
 });
-
 //reset round - done
 var resetRound = function(){
 	noOfMoves = 1;
@@ -88,7 +130,6 @@ var resetMatch = function(){
 	playerTwoMoves = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 	gameInProgress = true;
 };
-
 //adds mini image to scoreboard, displays winner message
 var roundOver = function(player) {
 	gameInProgress = false;
@@ -97,62 +138,58 @@ var roundOver = function(player) {
 		playerOneWins += 1;
 		if ( !endGame() ) {
 				winnerMessage1();
+				bWin.play();
+				clapAudio.play();
 		}
 	} else if (player === "Player Two") {
 		$(".game" + roundNumber).addClass('wolf');
 		playerTwoWins += 1;
 		if ( !endGame() ) {
 				winnerMessage2();
+				wWin.play();
+				clapAudio.play();
 		}
 	} else if (noOfMoves === 9){
-		($(".game" + roundNumber).addClass('red'));
-		drawMessage();
+			($(".game" + roundNumber).addClass('red'));
+				drawMessage();
+				panda.play();
+				boo.play();
 	}
 	roundNumber += 1;
 };
-
 //badger win message - limit of sweetAlert
 var winnerMessage1 = function (){
-	window.setTimeout(function() {
 		swal({   title: "Win",
 		text: "Battered by Badger!",
 		imageUrl: "Badger.png",
-		timer: 2000,
 		showConfirmButton: true,
 		confirmButtonText: "Next round?",
 		closeOnConfirm: true});
 	window.setTimeout(function() {
 		resetRound();
-	} ,3000);
-	});
+	}, 3000);
 };
 //wolf win message - limit of sweetAlerts
 var winnerMessage2 = function (){
-	window.setTimeout(function() {
 		swal({   title: "Win",
 		text: "A win for the Wolf!",
 		imageUrl: "Wolf.png",
-		timer: 2000,
 		showConfirmButton: true,
 		confirmButtonText: "Next round?",
 		closeOnConfirm: true});
 		window.setTimeout(function() {
 		resetRound();
 	} , 3000);
-	});
 };
 //draw message - limit of sweetAlert
 var drawMessage = function (){
-	window.setTimeout(function() {
 		swal({   title: "A Draw?!",
 		text: "The Panda of despire!",
 		imageUrl: "Red.png",
-		timer: 2000,
-		showConfirmButton: false});
+		showConfirmButton: true});
 		window.setTimeout(function() {
 		resetRound();
 	} , 3000);
-	});
 };
 //game logic derived from self populating arrays (above)
 var checkWinner = function(){
@@ -174,7 +211,6 @@ var checkWinner = function(){
 };
 //end messages
 var endGame = function(){
-	console.log("end game " + playerOneWins);
 	if (playerOneWins === 2) {
 		swal({   title: "BADGER WINS THE MATCH!",
 		text: "A black and white hero!",
@@ -182,6 +218,8 @@ var endGame = function(){
 		showConfirmButton: true,
 		confirmButtonText: "Another Match?",
 		closeOnConfirm: true});
+		winner.play();
+		clapAudio.play();
 		window.setTimeout(function() {
 		resetMatch();
 		} , 3000);
@@ -191,6 +229,8 @@ var endGame = function(){
 		text: "A lone wolf no longer!",
 		imageUrl: "Wolf.png",
 		showConfirmButton: true});
+		winner.play();
+		clapAudio.play();
 		window.setTimeout(function() {
 		resetMatch();
 		} , 3000);
