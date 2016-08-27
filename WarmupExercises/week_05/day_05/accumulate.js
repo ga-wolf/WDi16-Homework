5307396fe4fa168b4515b4aeeba34fdfca9c7020
Chris Barnaby <cjbarnaby@gmail.com>
2016-08-27 14:55:43 +1000

    Update accumulate.js

--- WarmupExercises/week_05/day_05/accumulate.js
+++ WarmupExercises/week_05/day_05/accumulate.js
@@ -17,12 +17,13 @@ var accumulate = {
 
 };
 
+var each = function(array, callback) {
+  for (var i = 0; i < array.length; i++) {
+    callback(array[i]);
+  }
+};
+  
 var accumulator = function(array) {
-  var each = function(array, callback) {
-    for (var i = 0; i < array.length; i++) {
-      callback(array[i]);
-    }
-  };
 
   for (var meth in accumulate) {
     results[meth] = [];
